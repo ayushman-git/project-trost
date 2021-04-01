af65d5ea3d1f7c8d6ab87d7c9dd747351bd9de08
Ayushman Gupta <ayushman1998.1998@gmail.com>
2021-04-01 12:48:33 +0530

    added weapon handler

---
+++ Assets/Scripts/WeaponHandler.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponHandler : MonoBehaviour
+{
+  public int selectedWeapon = 0;
+
+  void Start()
+  {
+    changeWeapon();
+  }
+  void Update()
+  {
+    if (Input.GetKeyDown(KeyCode.Alpha1))
+    {
+      selectedWeapon = 0;
+      changeWeapon();
+    }
+    if (Input.GetKeyDown(KeyCode.Alpha2))
+    {
+      selectedWeapon = 1;
+      changeWeapon();
+    }
+  }
+
+  void changeWeapon()
+  {
+    int i = 0;
+    foreach (Transform weapon in transform)
+    {
+      Debug.Log(i);
+      if (i == selectedWeapon)
+      {
+        weapon.gameObject.SetActive(true);
+      }
+      else
+      {
+        weapon.gameObject.SetActive(false);
+      }
+      i++;
+    }
+  }
+}
